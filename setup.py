eb44afaf65dca602cb15c01901fc92835a300ef7
mudit181 <mudit181bhatt@gmail.com>
2023-05-24 18:54:10 +0530

    all files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name="cel_income",
+version="0.0.1",
+author="mudit",
+author_email="mudit181bhatt@gmail.com",
+packages= find_packages(),
+install_requires=["pandas","numpy", "flask"]
+)
+
